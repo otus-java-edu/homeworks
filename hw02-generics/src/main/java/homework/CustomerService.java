1195eec5560f37576790cb09ef805e4065ee3738
Sergey Fokin <fsergeyal@gmail.com>
2023-01-24 11:13:36 +0300

    hw02-generics: copy-paste optimizations

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -11,29 +11,22 @@ public class CustomerService {
             (int)(o1.getScores() - o2.getScores()));
 
     public Map.Entry<Customer, String> getSmallest() {
-        var found = customerStringTreeMap.firstEntry();
-        if (found == null)
+        return cloneEntry(customerStringTreeMap.firstEntry());
+    }
+    private Map.Entry<Customer, String> cloneEntry(Map.Entry<Customer, String> entry)
+    {
+        if (entry == null)
             return null;
-        var foundCustomer = found.getKey();
+        var foundCustomer = entry.getKey();
         var result = new AbstractMap.SimpleImmutableEntry<>(
                 new Customer(foundCustomer.getId(),
                         foundCustomer.getName(),
                         foundCustomer.getScores()),
-                found.getValue());
+                entry.getValue());
         return result;
     }
-
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        var found = customerStringTreeMap.higherEntry(customer);
-        if (found == null)
-            return null;
-        var foundCustomer = found.getKey();
-        var result = new AbstractMap.SimpleImmutableEntry<>(
-                new Customer(foundCustomer.getId(),
-                        foundCustomer.getName(),
-                        foundCustomer.getScores()),
-                found.getValue());
-        return result;
+        return cloneEntry(customerStringTreeMap.higherEntry(customer));
     }
 
     public void add(Customer customer, String data) {
