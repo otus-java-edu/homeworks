b35f49827d6a9569877d469539e751e0cb9a7984
Sergey Fokin <fsergeyal@gmail.com>
2023-02-02 12:17:43 +0300

    fix comments

--- hw03-test-framework/src/main/java/Tester.java
+++ hw03-test-framework/src/main/java/Tester.java
@@ -1,71 +1,96 @@
 import Annotations.After;
 import Annotations.Before;
 import Annotations.Test;
+import lombok.Getter;
 
+import java.io.PrintStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Tester {
-    public static void main(String[] args){
-        if (args.length < 1)
-            return;
-        Class c = null;
-        try {
-            c = Class.forName(args[0]);
-        } catch (ClassNotFoundException e) {
-            System.out.println(String.format("Class %s not found", args[0]));
-        }
-        if (c!= null) {
-            var allMethods = c.getDeclaredMethods();
-            var beforeMethod = Arrays.stream(allMethods).filter(m->m.isAnnotationPresent(Before.class)).findFirst();
-            var testMethods = Arrays.stream(allMethods).filter(m->m.isAnnotationPresent(Test.class)).collect(Collectors.toUnmodifiableList());
-            var afterMethod = Arrays.stream(allMethods).filter(m->m.isAnnotationPresent(After.class)).findFirst();
-            Constructor constructor = null;
-            try {
-                constructor = c.getDeclaredConstructor();
-                constructor.setAccessible(true);
-            } catch (NoSuchMethodException e) {
-                System.out.println("Error - There is no default constructor");
-                return;
-            }
-            var passedTests = 0;
-            var failedTests = 0;
-            for (var test : testMethods) {
-                Object instance = null;
-                try {
-                    instance = constructor.newInstance();
-                } catch (Exception e) {
-                    System.out.println(String.format("Error on Instantiating by constructor: %s", e.getMessage()));
-                    failedTests++;
-                    break;
-                }
-                if (!executeMethod(instance, beforeMethod)) {
-                    executeMethod(instance, afterMethod);
-                    failedTests++;
-                    break;
-                }
-                if (executeMethod(instance, Optional.ofNullable(test))) {
-                    System.out.println(String.format("Test %s is passed", test.getName()));
-                    passedTests++;
-                }
-                else {
-                    System.out.println(String.format("Test %s is failed", test.getName()));
-                    failedTests++;
-                }
-                executeMethod(instance, afterMethod);
-            }
 
-            System.out.println("==========================================================================");
-            System.out.println(String.format("Total processed tests: %d", passedTests + failedTests));
-            System.out.println(String.format("Passed tests: %d", passedTests));
-            System.out.println(String.format("Failed tests: %d", failedTests));
-        }
+    private Class clazz;
+    private Optional<Method> beforeMethod;
+    private Collection<Method> testMethods;
+    private Optional<Method> afterMethod;
+
+    private Constructor constructor;
+
+    private boolean isInitialized = false;
+
+    private PrintStream outStream;
+
+    @Getter
+    private int passedTests;
+    @Getter
+    private int failedTests;
+
+    public Tester(PrintStream outStream){
+        this.outStream = outStream;
     }
 
-    private static boolean executeMethod(Object obj, Optional<Method> method)
+    public boolean Initialize(String className){
+        try {
+            passedTests = 0;
+            failedTests = 0;
+            clazz = Class.forName(className);
+
+            var allMethods = clazz.getDeclaredMethods();
+            beforeMethod = Arrays.stream(allMethods).filter(m->m.isAnnotationPresent(Before.class)).findFirst();
+            testMethods = Arrays.stream(allMethods).filter(m->m.isAnnotationPresent(Test.class)).collect(Collectors.toUnmodifiableList());
+            afterMethod = Arrays.stream(allMethods).filter(m->m.isAnnotationPresent(After.class)).findFirst();
+            try {
+                constructor = clazz.getDeclaredConstructor();
+                constructor.setAccessible(true);
+            } catch (NoSuchMethodException e) {
+                outStream.println("Error - There is no default constructor");
+                return false;
+            }
+        } catch (ClassNotFoundException e) {
+            outStream.println(String.format("Class %s not found", className));
+            return false;
+        }
+        isInitialized = true;
+        return true;
+    }
+
+    public void ExecuteTests()
+    {
+        for (var test : testMethods) {
+            Object instance = null;
+            try {
+                instance = constructor.newInstance();
+            } catch (Exception e) {
+                outStream.println(String.format("Error on Instantiating by constructor: %s", e.getMessage()));
+                failedTests++;
+                break;
+            }
+            if (!executeMethod(instance, beforeMethod)) {
+                executeMethod(instance, afterMethod);
+                failedTests++;
+                break;
+            }
+            if (executeMethod(instance, Optional.ofNullable(test))) {
+                outStream.println(String.format("Test %s is passed", test.getName()));
+                passedTests++;
+            }
+            else {
+                outStream.println(String.format("Test %s is failed", test.getName()));
+                failedTests++;
+            }
+            executeMethod(instance, afterMethod);
+        }
+
+        outStream.println("==========================================================================");
+        outStream.println(String.format("Total processed tests: %d", passedTests + failedTests));
+        outStream.println(String.format("Passed tests: %d", passedTests));
+        outStream.println(String.format("Failed tests: %d", failedTests));
+    }
+    private boolean executeMethod(Object obj, Optional<Method> method)
     {
         if (method.isPresent()) {
             try {
@@ -73,7 +98,7 @@ public class Tester {
                 m.setAccessible(true);
                 m.invoke(obj);
             } catch (Exception e) {
-                System.out.println(String.format("Error on method %s: %s",
+                outStream.println(String.format("Error on method %s: %s",
                         method.get().getName(), e.getMessage()));
                 return false;
             }
