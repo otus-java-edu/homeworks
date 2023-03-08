4dae5888e05fbb9f68973fb5bb687abe9acb28bd
Sergey Fokin <fsergeyal@gmail.com>
2023-03-08 10:59:12 +0300

    hw07-message-processor

---
+++ hw07-message-processor/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -0,0 +1,25 @@
+package ru.otus.listener.homework;
+
+import ru.otus.listener.Listener;
+import ru.otus.model.Message;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class HistoryListener implements Listener, HistoryReader {
+
+    private final Map<Long, Message> historyMap;
+    public HistoryListener(){
+        historyMap = new HashMap<>();
+    }
+    @Override
+    public void onUpdated(Message msg) {
+        historyMap.put(msg.getId(), msg.toBuilder().build());
+    }
+
+    @Override
+    public Optional<Message> findMessageById(long id) {
+        return Optional.ofNullable(historyMap.get(id));
+    }
+}
