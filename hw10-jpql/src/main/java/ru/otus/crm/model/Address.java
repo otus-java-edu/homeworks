34ee040ac4ce6b4a628b631f905d6136ce440abe
Sergey Fokin <fsergeyal@gmail.com>
2023-04-04 20:16:41 +0300

    hw10-jpql

---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Address.java
@@ -0,0 +1,30 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @SequenceGenerator(name = "address_gen", sequenceName = "address_seq",
+            initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "address_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "address")
+    private String address;
+
+    @Override
+    public Address clone() {
+        return new Address(this.id, this.address);
+    }
+}
