7e162bbf803701c996e8b1313810a3321a26f5a8
Sergey Fokin <fsergeyal@gmail.com>
2023-04-16 13:05:34 +0300

    hw11-cache

---
+++ hw11-cache/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,68 @@
+package ru.otus.crm.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @SequenceGenerator(name = "client_gen", sequenceName = "client_seq",
+            initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "client")
+    private List<Phone> phones;
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = phones;
+        phones.forEach(p->p.setClient(this));
+    }
+    @Override
+    public Client clone() {
+        var client = new Client(this.id, this.name);
+        if (this.address != null)
+            client.address = this.address.clone();
+        if (this.phones != null)
+            client.phones = this.phones.stream().map(p->{var n = p.clone(); n.setClient(client); return n;}).toList();
+        return client;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
