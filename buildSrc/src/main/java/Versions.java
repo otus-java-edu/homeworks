86ab8bfe452d2e8f2f6172074da7db38a6873366
Sergey Fokin <fsergeyal@gmail.com>
2023-06-06 11:18:00 +0300

    hw18-reactive

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -12,7 +12,9 @@ public interface Versions {
     String cassandra = "4.15.0";
     String neo4j = "5.7.0";
     String jedis = "4.3.2";
-
+    String sockjs = "1.5.1";
+    String stomp = "2.3.4";
+    String bootstrap = "5.2.3";
     String jetty = "11.0.14";
     String freemarker = "2.3.32";
 
@@ -21,4 +23,5 @@ public interface Versions {
     String errorProneAnnotations = "2.11.0";
     String tomcatAnnotationsApi = "6.0.53";
     String grpc = "1.44.1";
+    String r2dbcPostgresql = "0.8.13.RELEASE";
 }
